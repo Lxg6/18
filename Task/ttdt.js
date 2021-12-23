031e9b6c662a215efd690237d30f00e6aaa64cf9
Lxg <85028732+Lxg6@users.noreply.github.com>
2021-12-23 14:34:07 +0800

    Add files via upload

--- Task/ttdt.js
+++ Task/ttdt.js
@@ -20,7 +20,8 @@ let index
 $.message = "";
 imeiarr = [
    
-    "A7911E01-5ABB-4880-82FC-4757F300719F"
+    "202118AC-3BEB-45E0-80CB-69C7E5EFCB86",
+    "810ECFA0-970A-4EFF-92B4-8223A4F33209"
 ]
 !(async () => {
     await all();
