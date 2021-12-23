92b6024f92885f0e85750180d48c8ba8d14070c8
Lxg <85028732+Lxg6@users.noreply.github.com>
2021-12-23 14:37:36 +0800

    Update ttdt.js

--- Task/ttdt.js
+++ Task/ttdt.js
@@ -4,7 +4,7 @@ const jobname = 'ttdt'
 const { timeStamp } = require('console');
 const { MD5 } = require('crypto-js');
 const $ = Env(jobname)
-var sd = require('silly-datetime');
+//var sd = require('silly-datetime');
 let articlebody = require("../JSON/Youth_common.json");
 let currenttime;
 let md5str;
